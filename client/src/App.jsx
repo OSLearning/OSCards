0b0b20cf20d3a40a9112417c98a248c2c6ca83bc
Tolan Thornton <taoantaoan@hotmail.com>
2021-02-13 14:09:03 -0700

    Update json and webpack config; Test React components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return <div>hello world!</div>;
+    return (
+      <div>hello there</div>
+    )
   }
 }
 
