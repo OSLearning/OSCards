6a23f735e19039257ea2a5a607bec45b9be86ec5
Tolan Thornton <taoantaoan@hotmail.com>
2021-02-15 23:18:26 -0700

    Implentation of intuitive ux design to accomodate a variety of different learning styles, centering the user experience

--- client/src/pages/AddCard/AddCard.jsx
+++ client/src/pages/AddCard/AddCard.jsx
@@ -57,60 +57,33 @@ class AddCard extends Component {
   render () {
     let context = this;
     return (
-        <div className='addcard'>
+      <div className='addcard'>
         {/* // card form h2 "CARD FORM" */}
         <h2 className="addcard">Create A New Flash Card</h2>
         <form className="cardform">
-            <div  className="mb-3">
-              <label htmlFor="formGroupExampleInput"  className="form-label">Term</label>
-              <input type="text"  className="form-control" id="formGroupExampleInput" placeholder="YetiCrab" onChange={(e) => context.handleTerm(event)}/>
-            </div>      
-            <div  className="mb-3">
-              <label htmlFor="formGroupExampleInput2"  className="form-label">Definition</label>
-              <input type="text"  className="form-control" id="formGroupExampleInput2" placeholder="Kiwa hirsuta is a crustacean discovered in 2005 in the South Pacific Ocean. This decapod, which is approximately 15 cm long, is notable for the quantity of silky blond setae covering its pereiopods." onChange={(e) => context.handleDefinition(event)}/>
-            </div>
-            <select className="form-select" onChange={(e) => context.handleDeck(event)}>
-              <option>Select Your Deck</option>
-              <option defaultValue={1}>{1}</option>
-              <option defaultValue={2}>{2}</option>
-              <option defaultValue={3}>{3}</option>
-              <option defaultValue={4}>{4}</option>
-              <option defaultValue={4}>{5}</option>
-            </select>
+          <div  className="mb-3">
+            <label htmlFor="formGroupExampleInput"  className="form-label">Term</label>
+            <input type="text"  className="form-control" id="formGroupExampleInput" placeholder="YetiCrab" onChange={(e) => context.handleTerm(event)}/>
+          </div>      
+          <div  className="mb-3">
+            <label htmlFor="formGroupExampleInput2"  className="form-label">Definition</label>
+            <input type="text"  className="form-control" id="formGroupExampleInput2" placeholder="Kiwa hirsuta is a crustacean discovered in 2005 in the South Pacific Ocean. This decapod, which is approximately 15 cm long, is notable for the quantity of silky blond setae covering its pereiopods." onChange={(e) => context.handleDefinition(event)}/>
+          </div>
+          <select className="form-select" onChange={(e) => context.handleDeck(event)}>
+            <option>Select Your Deck</option>
+            <option defaultValue={1}>{1}</option>
+            <option defaultValue={2}>{2}</option>
+            <option defaultValue={3}>{3}</option>
+            <option defaultValue={4}>{4}</option>
+            <option defaultValue={4}>{5}</option>
+          </select>
         </form>
-        <button class="btn btn-primary" type="submit" onClick={context.handleSubmit}>+</button>
+        <div className="d-flex flex-row justify-content-center align-content-center mt-3">
+          <button class="btn btn-primary" type="submit" onClick={context.handleSubmit}>+</button>
         </div>
-            )  
-        }
-    }
+      </div>
+    )
+  }
+}
 
 export default AddCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*{ <label htmlFor="term">Term :</label> <br></br>
-<span><input type="text" className="termvalue"/> </span>    
-<p></p>
-<label htmlFor="definition">Definition :</label> <br></br>
-<span><input type="text" className="termvalue"/> </span> 
-<p></p>
-<label htmlFor="deck">Deck Label :</label> <br></br>
-<span><input type="text" className="termvalue"/> </span>  }*/
