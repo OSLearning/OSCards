9f99e2bd2da52a7f909d33f2b45f9ef4507c7d5e
Ramtin Khoee <ramtin.khoee@gmail.com>
2021-02-15 17:25:44 -0800

    Backend notecard routing set up and basic page routing implemented + skeleton of an AUTHORIZATION page implemented

---
+++ client/src/pages/Deck/Deck.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import "./Deck.css";
+import axios from 'axios';
+
+
+// create a page to view deck one cards, and create a get request to our DB
+// upon component did mount GET all cards in deck 1
+class Deck extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          deck: [{_id:123123123, term: 'name', definition: 'george', deck: '1'},{_id:123123123, term: 'name2', definition: 'kevin', deck: 1},]
+      }
+  }
+     
+  // componentDidMount() {
+  //   const {deckNumber} = this.props.location.state
+
+  //   axios.get(`/deck/${deckNumber}`)
+  //     .then((res) => {
+  //       console.log("res: ", res);
+  //       this.setState({deck: res})
+  //     })
+  //     .catch((err) => {
+  //       console.log("err: ", err);
+  //   })
+
+  // }
+  
+  render(){
+    // iterate through the response array of objects (this.state.deck)
+    // render a component for each card object
+    // ### provide flashcard styling
+    // ### provide flashcard functionality
+    const inputArray = this.state.deck;
+    const componentsToRender = [];
+    
+    inputArray.map((current, i) => {
+        componentsToRender.push(
+          <div key={i} className="flashcard">
+            <h5 className="card-title">{current.term}</h5>
+            <p className="card-text">{current.definition}</p>
+
+            <button type="button" className="btn btn-primary btn-sm">delete card</button>
+            <button type="button" className="btn btn-primary btn-sm">update card</button>
+          </div>
+        )
+    })
+    
+    return (
+        <div>
+            {componentsToRender}
+        </div>
+
+)
+}
+}
+
+
+export default Deck;
+
